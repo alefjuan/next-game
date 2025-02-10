9cc31a13c31a68d82645cd99ebed8fc1cc8377bc
Alef Juan <alefjuan88@gmail.com>
2025-02-10 15:28:00 -0300

    Excluindo Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  // Habilitar validação global
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Remove campos não definidos no DTO
-    forbidNonWhitelisted: true, // Lança erro se campos não definidos forem enviados
-    transform: true, // Transforma automaticamente os objetos no tipo correto
-  }));
-
-  // Configurar Swagger
-  const config = new DocumentBuilder()
-    .setTitle('Next Game API')
-    .setDescription('API para gerenciamento de jogos e listas')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+    // Inicia o servidor na porta 4000
+    await app.listen(3000);
 }
+
 bootstrap();
